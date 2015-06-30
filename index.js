bf0efbfd2060d631f628f5be5b1a20cf5dd98643
Pavel Tretyakov <tretjakov.pavel@gmail.com>
2015-06-30 16:10:04 +0000

    Переовожу систему на socket.io storage  Changes to be committed: 	modified:   index.js 	modified:   node_modules/chat/index.js 	modified:   package.json

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var Chat = new chat(io);
 io.on('connection', function(socket) {
 
 	socket.on('addClient', function(data) {
+		debug('добавляем клиента');
 		// Добавляем клиента
 		Chat.addClient(socket , data);
 		// Добавлеяем в комнату
@@ -18,7 +19,7 @@ io.on('connection', function(socket) {
 	});
 
 	socket.on('addConsultant' , function(data){
-		Chat.addConsultant(socket);
+		Chat.addConsultant(socket , data);
 		// Добавляем в комнату
 		socket.join('consultants');
 		// Отравлем пользователям что подключился новый консультант
@@ -32,10 +33,55 @@ io.on('connection', function(socket) {
 	socket.on('disconnect' , function(){
 		Chat.removeConnection(socket);
 		return io.emit('takeCountConsultants' , {count : Chat.issetConsultants()});
-	})
+	});
+	
+	// Прослушки для синхронизации с backbone
+	socket.on('create', function (data) {
+        create(socket, data.signature);       
+    });      
+    socket.on('read', function (data) {
+        read(socket, data.signature);
+    });  
+    socket.on('update', function (data) {
+        update(socket, data.signature);       
+    }); 
+    socket.on('delete', function (data) {
+        destroy(socket, data.signature);       
+    });
 
 });
 
-debug('Запущен сервер http://127.0.0.1:1337/');
+debug('Запущен сервер http://127.0.0.1:1337/ ' + process.env.PORT + ' ' + process.env.IP);
 
-app.listen(1337);
+app.listen(process.env.PORT);
+
+// Создаем методы для синхронизации с сервером
+var create = function (socket, signature) {
+    var e = event('create', signature), data = [];
+    socket.emit(e, {id : 1});            
+};
+ 
+var read = function (socket, signature) {
+    var e = event('read', signature), data;
+    data.push({})
+    socket.emit(e, data);            
+};
+ 
+var update = function (socket, signature) {
+    var e = event('update', signature), data = [];
+    socket.emit(e, {success : true});            
+};
+ 
+var destroy = function (socket, signature) {
+    var e = event('delete', signature), data = [];
+    socket.emit(e, {success : true});            
+};
+ 
+// creates the event to push to listening clients
+var event = function (operation, sig) {
+    var e = operation + ':'; 
+    e += sig.endPoint;
+    if (sig.ctx) e += (':' + sig.ctx);
+ 
+    return e;
+};
